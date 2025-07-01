567a41d76de67a3e7c65f7300b2f70ddbb9a3a1c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-07-01 15:36:10 +0200

    Correction Jeu

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -6,6 +6,25 @@
 // 4 - Stocker les essais dans un tableau et les réafficher entre chaque tour (API Array sur MDN)
 // 5 - Afficher une erreur si la saisie n'est pas un nombre (API Number sur MDN)
 // Attention, le callback de question est toujours appelé avec un type String, à convertir si besoin.
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
 
 const readline = require("readline");
 
@@ -15,17 +34,37 @@ const rl = readline.createInterface({
 });
 
 function jouer() {
+  if (essais.length) {
+    console.log(`Vos essais précédents : ${essais.join(", ")}`);
+  }
+
   rl.question("Devinez un nombre entre 0 et 100 : ", (answer) => {
     console.log(`Vous avez saisi : ${answer}`);
 
-    // Trouvé, quitter :
-    rl.close();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // Pas trouvé, reposer la question, c'est à dire revenir à la ligne 17
-    jouer();
+    if (Number.isNaN(entierSaisi)) {
+      console.error("Erreur : Ce n'est pas un nombre valide !");
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log("C'est plus grand !");
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log("C'est plus petit !");
+      jouer();
+    } else {
+      console.log("Bravo, vous avez trouvé le nombre !");
+      rl.close(); // Fermer l'interface readline
+    }
   });
 }
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
 
 // Pile d'appels
